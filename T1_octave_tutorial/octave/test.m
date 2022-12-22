604f73220a78c227fe9b897fbaace91cbf5c0f7a
UrMBCMRabbont <philipt710mh@gmail.com>
2022-12-22 10:55:27 +0800

    familar with octave

--- T1_octave_tutorial/octave/test.m
+++ T1_octave_tutorial/octave/test.m
@@ -17,9 +17,29 @@ Y = [A2 A2];
 Z = repmat(A,2,3);
 
 u = unifrnd(5, 15, 10, 1);
+N = normrnd(2.5, 5.0, 5, 5);
+%%%%%%%% MATRIX OPERATION
+dotA = A*A';
+elementA = A .* A';
+fuor_tens = 10+zeros(4);
 %%%%%%%% PROGRAMMING STRUCT %%%%%%%%
+function funcans = add_func(a, b)
+  funcans = a + b
+end  
+add_func(E, bb);
 
-
+for i = [1:10]
+  if i == 10
+    disp(i)
+  else
+    disp(i-10)
+  endif
+  
+endfor
+disp('hahaha')
+for i = 1:length(A(1))
+  disp(i)
+endfor
 %%%%%%%% PLOTTING %%%%%%%%
 x = -2*pi:0.1:2*pi;
 y = sin(x);
