8174837281177db6e798123a17f8fcc996777b3d
UrMBCMRabbont <philipt710mh@gmail.com>
2022-12-27 18:56:12 +0800

    full Jacobian revised

--- T4_EKFSlam/octave/prediction_step.m
+++ T4_EKFSlam/octave/prediction_step.m
@@ -5,6 +5,7 @@ function [mu, sigma] = prediction_step(mu, sigma, u)
 % u: odometry reading (r1, t, r2)
 % Use u.r1, u.t, and u.r2 to access the rotation and translation values
 
+n = size(sigma)(1);
 % TODO: Compute the new mu based on the noise-free (odometry-based) motion model
 % Remember to normalize theta after the update (hint: use the function normalize_angle available in tools)
 mu(1) = mu(1) + (u.t*cos(mu(3) + u.r1));
@@ -16,8 +17,9 @@ mu(3) = normalize_angle(mu(3, 1));
 Gx = eye(3);
 Gx(1, 3) = (-u.t*cos(mu(3) + u.r1));
 Gx(2, 3) = (-u.t*sin(mu(3) + u.r1));
+m = size(Gx)(1);
 % TODO: Construct the full Jacobian G
-G = [Gx zeros(3, n-3); zeros(n-3, 3) eye(n-3)];
+G = [Gx zeros(m, n-m); zeros(n-m, m) eye(n-m)];
 
 % Motion noise
 motionNoise = 0.1;
